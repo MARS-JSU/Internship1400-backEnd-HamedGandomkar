c35530ff6183f34437073a04bc81f0c943a170df
Hamed Gandomkar <dev.hamedgk@yahoo.com>
2021-09-17 03:27:16 +0430

    minor change

--- Project3/app/Utils/Analyzer.php
+++ Project3/app/Utils/Analyzer.php
@@ -6,14 +6,14 @@ use App\Contracts\Types\AnalyzerInterface;
 
 class Analyzer implements AnalyzerInterface
 {
-    private string $wholeExpression;
-    private array $arrayOfExpressions;
+    private string $rawString;
+    private array $strings;
     private array $arrayOfMonos;
     private string $firstChar;
 
     public function __construct(string $expression)
     {
-        $this->wholeExpression = $expression;
+        $this->rawString = $expression;
     }
 
     public function init()
@@ -24,21 +24,17 @@ class Analyzer implements AnalyzerInterface
         $this->analyzer();
         $this->insertRemovedChar();
         $this->packToMono();
-        if(isset($this->arrayOfMonos)){
-            return $this->arrayOfMonos;
-        }else{
-            throw new \Exception("uninitilized instance!");
-        }
+        return $this->arrayOfMonos;
     }
 
     private function handleFirstChar()
     {
-        if ($this->wholeExpression[0] == '-') {
+        if ($this->rawString[0] == '-') {
             $this->firstChar = '-';
-            $this->wholeExpression = ltrim($this->wholeExpression, '-');
-        } elseif ($this->wholeExpression[0] == '+') {
+            $this->rawString = ltrim($this->rawString, '-');
+        } elseif ($this->rawString[0] == '+') {
             $this->firstChar = '+';
-            $this->wholeExpression = ltrim($this->wholeExpression, '+');
+            $this->rawString = ltrim($this->rawString, '+');
         } else {
             $this->firstChar = '+';
         }
@@ -47,24 +43,24 @@ class Analyzer implements AnalyzerInterface
     private function insertRemovedChar()
     {
         if (isset($this->firstChar)) {
-            $this->arrayOfExpressions[0] = $this->firstChar . $this->arrayOfExpressions[0];
+            $this->strings[0] = $this->firstChar . $this->strings[0];
         }
     }
 
     private function makeCoefficientOne()
     {
-        $this->wholeExpression = str_replace(['-x', '+x'], ['-1x', '+1x'], $this->wholeExpression);
+        $this->rawString = str_replace(['-x', '+x'], ['-1x', '+1x'], $this->rawString);
     }
 
     private function seperation()
     {
-        $temp = str_replace(['+', '-'], [' +', ' -'], $this->wholeExpression);
-        $this->arrayOfExpressions = explode(' ', $temp);
+        $temp = str_replace(['+', '-'], [' +', ' -'], $this->rawString);
+        $this->strings = explode(' ', $temp);
     }
 
     private function analyzer()
     {
-        foreach ($this->arrayOfExpressions as &$mono) {
+        foreach ($this->strings as &$mono) {
             if (strpos($mono, 'x') === false) {
                 $mono = $mono . "x^0";
             } elseif (strpos($mono, 'x^') === false) {
@@ -76,7 +72,7 @@ class Analyzer implements AnalyzerInterface
     private function packToMono()
     {
         $generatedMonos = [];
-        foreach ($this->arrayOfExpressions as &$mono) {
+        foreach ($this->strings as &$mono) {
             $temp = explode('x^', $mono);
             $temp[0] = (float)$temp[0];
             $temp[1] = (float)$temp[1];
